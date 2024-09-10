b3440c5f707629f7d888bf4d0516d783e7ad3967
Alexey <dignezzz@gmail.com>
2024-09-10 23:51:17 +0300

    Update date.ts

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,23 +2,24 @@ const dateOptions = {
   locale: "ru-RU",
   options: {
     day: "numeric",
-    month: "short", // "short" для сокращенных названий месяцев
+    month: "short",
     year: "numeric",
   }
 };
 
-// Форматируем даты по умолчанию с помощью "ru-RU" локали
-const dateFormat = (locale: string, options: Intl.DateTimeFormatOptions) =>
-  new Intl.DateTimeFormat(locale, options);
+const dateFormat = new Intl.DateTimeFormat(
+  dateOptions.locale, 
+  dateOptions.options as Intl.DateTimeFormatOptions
+);
 
-// Функция для получения отформатированной даты
 export function getFormattedDate(
   date: string | number | Date,
   options?: Intl.DateTimeFormatOptions,
   locale?: string,
 ) {
-  const formatLocale = locale ?? dateOptions.locale;
-  const formatOptions = options ?? dateOptions.options;
+  if (typeof options !== "undefined") {
+    return new Date(date).toLocaleDateString(locale ?? dateOptions.locale, options);
+  }
 
-  return dateFormat(formatLocale, formatOptions).format(new Date(date));
+  return dateFormat.format(new Date(date));
 }
