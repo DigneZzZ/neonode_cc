45c50f1e0733531a736e4b051f4f4d2b7a99799b
Alexey <dignezzz@gmail.com>
2024-09-11 08:33:11 +0300

    даты

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,25 +1,30 @@
+// Опции по умолчанию для русской локали
 const dateOptions = {
   locale: "ru-RU",
   options: {
     day: "numeric",
-    month: "short",
+    month: "short",  // "short" - месяц в коротком формате (например, "сент.")
     year: "numeric",
-  }
+  } as Intl.DateTimeFormatOptions,
 };
 
+// Создание формата даты по умолчанию
 const dateFormat = new Intl.DateTimeFormat(
   dateOptions.locale, 
-  dateOptions.options as Intl.DateTimeFormatOptions
+  dateOptions.options
 );
 
+// Функция для форматирования даты
 export function getFormattedDate(
   date: string | number | Date,
   options?: Intl.DateTimeFormatOptions,
   locale?: string,
-) {
-  if (typeof options !== "undefined") {
-    return new Date(date).toLocaleDateString(locale ?? dateOptions.locale, options);
+): string {
+  // Если переданы опции или локаль, используем их, иначе - по умолчанию
+  if (options || locale) {
+    return new Date(date).toLocaleDateString(locale ?? dateOptions.locale, options ?? dateOptions.options);
   }
 
+  // Если опции не переданы, используем формат по умолчанию
   return dateFormat.format(new Date(date));
 }
