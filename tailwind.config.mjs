3a4eab0e41ca4c0255f31b333c6f025bc91373bf
Alexey <dignezzz@gmail.com>
2024-09-12 15:20:56 +0300

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
-	  fontFamily: {
-		'mono': ['Anonymous Pro'], // Добавляем Anonymous Pro
-	  },
 	  extend: {
+		fontSize: {
+		  '1.05rem': ['1.05rem', {lineHeight: '1.75rem'}]
+		},
+		fontFamily: {
+		  'mono': ['Anonymous Pro']
+		},
 		colors: {
 		  bgColor: "var(--theme-bg)",
 		  textColor: "var(--theme-text)",
 		  link: "var(--theme-link)",
 		  accent: "var(--theme-accent)",
-		  "accent-2": "var(--theme-accent-2)",
+		  'accent-2': "var(--theme-accent-2)",
 		  surface: "var(--theme-surface)",
 		  quote: "var(--theme-quote)",
 		  highlight: "var(--theme-highlight)"
